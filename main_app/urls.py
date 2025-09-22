4ce567ec969abde2e3102434c41d8046f5857042
fatemaalalaiwi <F.alalaiwi@gmail.com>
2025-09-22 14:13:41 +0300

    css for Base(animation)

--- main_app/urls.py
+++ main_app/urls.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.urls import path
 from . import views
 from django.contrib.auth import views as auth_views
-# urls.py
+
 
 urlpatterns = [
     path('', views.home, name='home'),
@@ -11,16 +11,12 @@ urlpatterns = [
     path('games/create' ,views.GameCreate.as_view(), name='games_create'),
     path('games/<int:pk>/update/', views.GameUpdate.as_view(), name='games_update'),
     path('games/<int:pk>/delete/', views.GameDelete.as_view(), name='games_delete'),
-    # path('games/<int:game_id>/add_feeding/', views.add_feeding, name='add_feeding'),
 
-
-
-    # CBV's fpt Kids Model
+    # Dashboard and game_dashboard
     path('dashboard/', views.dashboard, name='dashboard'),
-    # urls.py
     path('game_dashboard/<int:game_id>/', views.game_dashboard, name='game_dashboard'),
 
-
+    # CBV's fpt Kids Model
     path('kids/', views.KidList.as_view(), name='kid_index'),
     path('kids/<int:pk>/', views.KidDetail.as_view(), name='kid_detail'),
     path('kids/create/', views.KidCreate.as_view(), name='kid_create'),
@@ -29,6 +25,7 @@ urlpatterns = [
 
     path('kids/<int:kids_id>/assoc_kid/<int:game_id>/' , views.assoc_kid, name='assoc_kid'),
     path('kids/<int:kids_id>/unassoc_kid/<int:game_id>/' , views.unassoc_kid, name='unassoc_kid'),
+    
     # ... To update the status after thime out
     path('update_kid_status/<int:kid_game_id>/', views.update_kid_status, name='update_kid_status'),
     path('nfc_generate/<str:hash>/<str:token>/', views.nfc_generate, name='nfc_generate'),
